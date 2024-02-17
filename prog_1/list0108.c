c81cd041e82dc3f95077e7a75a4fa07ecce24d76
NANDK <keitaro.noda@gmail.com>
2024-02-17 16:43:25 +0900

    -

---
+++ prog_1/list0108.c
@@ -0,0 +1,14 @@
+// 二つの変数に整数値を代入して表示
+
+#include <stdio.h>
+
+int main(void) {
+    int x, y;
+    x = 57;
+    y = x+12;
+
+    printf("xの値は%dです。\n", x);
+    printf("yの値は%dです。\n", y);
+
+    return 0;
+}
